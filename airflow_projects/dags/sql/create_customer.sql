ae27dd8586819434d909f15f63e33e5c71fec763
ansamAY <ansam.yousry@gmail.com>
2025-01-01 09:35:40 +0100

    Delete airflow_projects directory

--- airflow_projects/dags/sql/create_customer.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS customers (
-            customer_id varchar(50) NOT NULL,
-            customer_name VARCHAR(50) NOT NULL,
-            address VARCHAR(50) NOT NULL,
-            birth_date DATE NOT NULL
-            );
